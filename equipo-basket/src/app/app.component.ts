ea8ea785ba7287c3a08fb0ccfbaa2ac1b0c0e5c6
Omaima <okhoyani@uoc.edu>
2025-10-06 17:28:17 +0200

    Mejorando interfaz

--- equipo-basket/src/app/app.component.ts
+++ equipo-basket/src/app/app.component.ts
@@ -6,12 +6,13 @@ import { Player } from './models/player.model';
 import { PlayersComponent } from './components/players/players.component';
 import { DetailComponent }  from './components/detail/detail.component';
 import { MediaComponent }   from './components/media/media.component';
-import { SafeUrlPipe } from './pipes/safe-url.pipe';
+// (Opcional) SafeUrlPipe ya no es necesario si Media usa DomSanitizer
+// import { SafeUrlPipe } from './pipes/safe-url.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, PlayersComponent, DetailComponent, MediaComponent, SafeUrlPipe],
+  imports: [CommonModule, PlayersComponent, DetailComponent, MediaComponent], // quité SafeUrlPipe
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
@@ -21,5 +22,10 @@ export class AppComponent {
 
   onSelect(p: Player) {
     this.selectedPlayer = p;
+  }
+
+  // 👇 para cumplir la rúbrica: cerrar detalle y volver sin selección
+  onCloseDetail() {
+    this.selectedPlayer = null;
   }
 }
